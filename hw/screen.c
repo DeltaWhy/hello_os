1b27cdc647aafc7a19de4d24a76449c7fe7b8bbe
Michael Limiero <mike5713@gmail.com>
2013-10-23 18:05:10 -0400

    Clean up screen.c
    * cprint() could be faster - right now it needlessly updates the cursor
      after each character - but make sure not to write off the screen

--- hw/screen.c
+++ hw/screen.c
@@ -1,95 +1,66 @@
 #include "port.h"
 #include "screen.h"
+
 volatile unsigned char *videoram = (unsigned char *)0xB8000;
 volatile unsigned char *cursor = (unsigned char *)0xB8000;
-int crow, ccol;
-void update_cursor(){
 
-	unsigned short position=(crow*80) + ccol;
-	
+void update_cursor(){
+	unsigned short position=(cursor-videoram)/2;
+
 	outportb(0x3D4, 0x0F);
 	outportb(0x3D5, (unsigned char)(position&0xFF));
-	
+
 	outportb(0x3D4, 0x0E);
 	outportb(0x3D5, (unsigned char)((position>>8)&0xFF));
 }
+
 void kclear() {
     cursor = videoram;
     for (int i=0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
         cursor[0] = ' ';
         cursor[1] = 0x07;
         cursor += 2;
-		crow = 0;
-		ccol = 0;
-		update_cursor();
     }
     cursor = videoram;
+    update_cursor();
 }
+
 /*01 = darkblue 02 = green 03 = lightblue 04 = darkred 05 = darkpink 06 = orange 07 = lightgray 08 = darkgray 09 = purple 10 = babygreen 11 = lightred 12 = lightpink 13 = yellow 14 = white*/
 void cprint(char * str, char col) {
-	while (*str != 0) {
-		if (*str == '\n'){
-			int offset = cursor - videoram;
-			int column = offset % (SCREEN_WIDTH * 2);
-			cursor += (SCREEN_WIDTH * 2) - column;
-			ccol += ((SCREEN_WIDTH * 2) - column)/2;
-			update_cursor();
-			scroll();
-		}else{
-			kputc(*str, col);
-			ccol++;
-			update_cursor();
-		}
-		str++;
-	}
-}
-void charPrint(char str) {
-
-	if (str == '\n'){
-		int offset = cursor - videoram;
-		int column = offset % (SCREEN_WIDTH * 2);
-		cursor += (SCREEN_WIDTH * 2) - column;
-		ccol += ((SCREEN_WIDTH * 2) - column)/2;
-		update_cursor();
-		scroll();
-	}else{
-		kputc(str, 0x07);
-		ccol++;
-		update_cursor();
-	}
-
+    while (*str != 0) {
+        kputc(*str, col);
+        scroll();
+        update_cursor();
+        str++;
+    }
 }
 
 void print(char * str) {
-	while (*str != 0) {
-		if (*str == '\n'){
-			int offset = cursor - videoram;
-			int column = offset % (SCREEN_WIDTH * 2);
-			cursor += (SCREEN_WIDTH * 2) - column;
-			ccol += ((SCREEN_WIDTH * 2) - column)/2;
-			update_cursor();
-			scroll();
-		}else{
-			ccol++;
-			update_cursor();
-			kputc(*str, 0x07);
-		}
-		str++;
-	}
+    cprint(str, 0x07);
 }
-		
+
 
 void kputc(char c, char col) {
-
-    cursor[0] = c;
-    cursor[1] = col;
-    cursor += 2;
+    int offset, column;
+    switch(c) {
+        case '\n':
+            offset = cursor-videoram;
+            column = offset % (SCREEN_WIDTH * 2);
+            cursor += (SCREEN_WIDTH * 2) - column;
+            break;
+        default:
+            cursor[0] = c;
+            cursor[1] = col;
+            cursor += 2;
+    }
     scroll();
+    update_cursor();
 }
+
 void scroll(){
-int i;
-int offset = cursor - videoram;
-      if(offset >= 160*25)          /* Are we off-screen ?          */
+    int i;
+    int offset = cursor - videoram;
+      while(offset >= 160*25)          /* Are we off-screen ?          */
       {
          for(i = 0; i < 160*24; i++)    /* Scroll the screen up         */
          {
--- hw/screen.h
+++ hw/screen.h
@@ -3,21 +3,13 @@
 
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 26
-
-
-
-
-
 void update_cursor();
 void kclear();
 void kputc(char c, char col);
 void print(char * str);
 void cprint(char * str, char col);
-void charPrint(char str);
 void scroll();
 char * itoa( int value, char * str, int base );
 extern volatile unsigned char *videoram;
 extern volatile unsigned char *cursor;
-extern int ccol;
-extern int crow;
 #endif
