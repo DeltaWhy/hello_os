76411ca18b851c0628753a06ed22547e273ff75b
Michael Limiero <mike5713@gmail.com>
2013-11-10 23:38:52 -0500

    Add assert.h

---
+++ assert.h
@@ -0,0 +1,11 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+#include "sprintf.h"
+#include "globals.h"
+
+#ifdef NDEBUG
+#define assert(cond) ((void)0)
+#else
+#define assert(cond) do { if (!(cond)) printf("%s:%d: %s: Assertion `"#cond"' failed.\n", __FILE__, __LINE__, __func__); panic("Assertion failed"); } while (0)
+#endif
+#endif
