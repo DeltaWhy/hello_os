707698b43d4f1bf6984630d9b2881e6392d058d4
Michael Limiero <mike5713@gmail.com>
2013-11-19 00:35:01 -0500

    Filesystem and FAT structs

---
+++ fs/fat.h
@@ -0,0 +1,73 @@
+#ifndef FAT_H
+#define FAT_H
+#include <stdint.h>
+struct __attribute__ ((__packed__)) fat_bpb {
+    char ignore[3];
+    char oem[8];
+    uint16_t bytes_per_sector;
+    uint8_t sectors_per_cluster;
+    uint16_t reserved_sectors;
+    uint8_t num_fats;
+    uint16_t num_direntries;
+    uint16_t total_sectors;
+    uint8_t media_descriptor;
+    uint16_t sectors_per_fat; //fat12 and 16 only
+    uint16_t sectors_per_track;
+    uint16_t num_heads;
+    uint32_t num_hidden_sectors;
+    uint32_t total_sectors_large;
+};
+
+struct __attribute__ ((__packed__)) fat16_ebpb {
+    uint8_t drive_num;
+    uint8_t nt_flags;
+    uint8_t signature;
+    uint32_t vol_serial;
+    char vol_label[11]; //padded with spaces
+    char system_id[8]; //padded with spaces
+    char ignore[448];
+    uint16_t boot_sig;
+};
+
+struct __attribute__ ((__packed__)) fat32_ebpb {
+    uint32_t sectors_per_fat;
+    uint16_t flags;
+    uint16_t version;
+    uint32_t root_dir_cluster;
+    char reserved[12];
+    uint8_t drive_num;
+    uint8_t nt_flags;
+    uint8_t signature;
+    uint32_t vol_serial;
+    char vol_label[11];
+    char system_id[8];
+    char ignore[420];
+    uint16_t boot_sig;
+};
+
+struct __attribute__ ((__packed__)) fat_dir_entry {
+    char filename[11];
+    uint8_t attributes;
+    uint8_t reserved;
+    uint8_t created_time_tenths;
+    uint16_t created_time; //hour 5 bits, minute 6 bits, second 5 bits
+    uint16_t created_date; //year 7 bits, month 4 bits, day 5 bits
+    uint16_t accessed_date;
+    uint16_t first_cluster_hi;
+    uint16_t modified_time;
+    uint16_t modified_date;
+    uint16_t first_cluster_lo;
+    uint32_t size;
+};
+
+struct __attribute__ ((__packed__)) fat_lfn_entry {
+    uint8_t order;
+    uint16_t filename1[5];
+    uint8_t attributes;
+    uint8_t long_entry_type;
+    uint8_t checksum;
+    uint16_t filename2[6];
+    uint16_t zero;
+    uint16_t filename3[2];
+};
+#endif
---
+++ fs/fs.h
@@ -0,0 +1,55 @@
+#ifndef FS_H
+#define FS_H
+#include <stdint.h>
+#include "fs/block_dev.h"
+
+typedef int(*file_io_fn)(char *, uint64_t);
+
+enum FILE_FLAGS {
+    FILE_FLAG_READ = 0x1,
+    FILE_FLAG_WRITE = 0x2,
+    FILE_FLAG_APPEND = 0x4,
+    FILE_FLAG_CREATE = 0x8,
+    FILE_FLAG_TRUNCATE = 0x10
+};
+
+typedef struct file {
+    char *path;
+    uint64_t position;
+    uint32_t flags;
+    file_io_fn read;
+    file_io_fn write;
+} file_t;
+
+enum FILE_ATTRS {
+    FILE_ATTR_READONLY = 0x1,
+    FILE_ATTR_HIDDEN = 0x2,
+    FILE_ATTR_DIRECTORY = 0x4
+};
+
+typedef struct dir_entry {
+    char *name;
+    uint32_t attributes;
+    uint64_t size;
+} dir_entry_t;
+
+typedef dir_entry_t *(*dir_next_fn)();
+typedef struct dir {
+    char *path;
+    uint32_t position;
+    dir_next_fn next;
+} dir_t;
+
+typedef file_t *(*fs_fopen_fn)(char *, uint32_t);
+typedef dir_t *(*fs_diropen_fn)(char *);
+typedef struct file_system {
+    uint32_t id;
+    block_dev_t *device;
+    char * type;
+    char * label;
+    fs_fopen_fn open_file;
+    fs_diropen_fn open_dir;
+} file_system_t;
+
+file_system_t * fs_mount(block_dev_t *device);
+#endif
